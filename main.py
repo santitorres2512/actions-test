43726c9a4b4e8c40829ca4fb8e277d167b1bd22f
Santiago Torres <105827689+santitorres2512@users.noreply.github.com>
2022-10-14 10:18:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,4 @@ except KeyError:
 if __name__ == "__main__":
     logger.info(f"Token value: {SOME_SECRET}")
 
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+    print("It executes this part of the script")
