3d341fe28b11414445663cb8000fb77efe67d462
Santiago Torres <105827689+santitorres2512@users.noreply.github.com>
2022-10-14 11:10:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ import requests                                                                 
 from os import walk                                                            # library used to know the file names of the .jrxml files to be converted
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
 try:
     SOME_SECRET = os.environ["SOME_SECRET"]
 except KeyError:
