09cecdafdf1797a3e13b295a34d7065ed5d08bff
Santiago Torres <105827689+santitorres2512@users.noreply.github.com>
2022-10-14 10:46:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import logging.handlers
 import os
 
 import requests
+import time                                                                    # Time is used for waiting x amount of seconds, on this case is used for waiting for the Organization field to be displayed on Oneview's login                                                         
+from os import walk                                                            # library used to know the file names of the .jrxml files to be converted
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
